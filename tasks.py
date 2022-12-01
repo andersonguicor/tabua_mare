64f2d6bb7ddcb990a6fe3408fd275ba8d08a7516
andersonguicor <99292322+andersonguicor@users.noreply.github.com>
2022-12-01 19:28:57 -0300

    Inicio
    Script inicial

---
+++ tasks.py
@@ -0,0 +1,52 @@
+import tabula
+import pandas as pd
+from tabulate import tabulate
+
+
+url = "https://www.marinha.mil.br/chm/sites/www.marinha.mil.br.chm/files/dados_de_mare/41-porto_do_rio_de_janeiro.pdf"
+
+
+def extract_pdf(url: str) -> pd.DataFrame:
+
+    # https://tabula-py.readthedocs.io/en/latest/tabula.html
+
+    tabula.convert_into(
+        url,
+        "output.csv",
+        output_format="csv",
+        java_options="-Dfile.encoding=UTF8",
+        pages="all",
+        area=[
+            53.6,
+            27.8,
+            812.8,
+            94.6,
+        ],  # Portion of the page to analyze(top,left,bottom,right).
+        columns=[54.9, 77.8, 93.9],  # the end of column widith in pt
+        guess=False,
+        silent=True,
+    )
+
+    df = pd.read_csv("output.csv", sep=",", header=0)
+    df.columns = ["DIA", "HORA", "ALT (m)"]
+
+    df["HORA"] = df["HORA"].fillna("").astype(str)
+    df["HORA"] = df["HORA"].apply(lambda x: "" if x == "" else (x[0:2] + ":" + x[2:4]))
+
+    for i in range(len(df) - 1):
+        dia = df.loc[i][0]
+        if str(df.loc[i][0]).isnumeric():
+            df.loc[i][0] = dia
+        else:
+            df.loc[i][0] = df.loc[i - 1][0]
+
+    return df
+
+
+table = tabulate(
+    extract_pdf(url).head(10).fillna(""),
+    headers=["DIA", "HORA", "ALT (m)"],
+    tablefmt="grid",
+)
+
+print(table)
